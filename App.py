279ad86e851a5e61196fc5ded0553071d239c47f
V1RUSnya <49871848+V1RUSnya@users.noreply.github.com>
2023-09-29 17:27:17 +0300

    Update App.py
    task 3.2 completed

--- App.py
+++ App.py
@@ -2,13 +2,22 @@
 import sys
 import os
 import typing
+import cv2
+import imghdr
+import pathlib
 import matplotlib.pyplot as plt
 import numpy as np
+import idx2numpy
+import time
+from scipy import io as spio
+from keras import optimizers
+from keras import backend as K
 from keras.datasets import cifar10
 from keras.preprocessing import image
 from keras.models import Sequential, load_model
-from keras.layers import Dense, Dropout, Flatten, BatchNormalization, Activation
+from keras.layers import Dense, Dropout, Flatten, BatchNormalization, Activation, Reshape, LSTM
 from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.optimizers import SGD, RMSprop, Adam
 from keras.constraints import maxnorm
 from keras.utils import np_utils
 from keras.metrics import Accuracy
@@ -24,7 +33,7 @@ class Start(QMainWindow):
         super().__init__()
         
         self.setWindowTitle("BesDV") #Имя программы
-        self.setFixedSize(QSize(800, 500))
+        self.setFixedSize(QSize(1600, 900))
         
         #Добавляем виджеты
         self.label = QLabel()
@@ -32,8 +41,12 @@ class Start(QMainWindow):
         self.button.clicked.connect(self.get_photo)
         self.Mnistbutton = QPushButton("MNIST")
         self.Mnistbutton.clicked.connect(self.mnist_dataset)
-        self.Kerasbutton = QPushButton("Generate model")
+        self.Kerasbutton = QPushButton("Generate cifar10 model")
         self.Kerasbutton.clicked.connect(self.KerasImage)
+        self.ImagetoTextbutton = QPushButton("Scan text on image")
+        self.ImagetoTextbutton.clicked.connect(self.imgtotext)
+        self.TextGenbutton = QPushButton("Generate EMNIST model")
+        self.TextGenbutton.clicked.connect(self.emnist)
         
         #Создаем слой
         layout = QVBoxLayout()
@@ -41,6 +54,8 @@ class Start(QMainWindow):
         layout.addWidget(self.button)
         layout.addWidget(self.Kerasbutton)
         layout.addWidget(self.Mnistbutton)
+        layout.addWidget(self.ImagetoTextbutton)
+        layout.addWidget(self.TextGenbutton)
 
         #Создание макета
         self.container = QWidget()
@@ -188,7 +203,7 @@ class Start(QMainWindow):
         model.add(Activation('softmax'))
         # Наконец то компилируем
         # Алгоритм Адама является одним из наиболее часто используемых оптимизаторов, потому что он дает высокую производительность в большинстве задач
-        epochs = 25
+        epochs = 100
         optimizer = 'adam'
         model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
         #print(model.summary()) # Вывод данных для отладки
@@ -205,6 +220,156 @@ class Start(QMainWindow):
         self.button.setStyleSheet('background-color: #111111; color: white;')
         self.Kerasbutton.setStyleSheet('background-color: #111111; color: white;')
         self.Mnistbutton.setStyleSheet('background-color: #111111; color: white;')
+        self.ImagetoTextbutton.setStyleSheet('background-color: #111111; color: white;')
+        self.TextGenbutton.setStyleSheet('background-color: #111111; color: white;')
+    
+    def emnist(self):
+        folder_path = QFileDialog.getExistingDirectory(self, "Choose folder")
+        print(folder_path)
+        emnist_path = folder_path
+        emnist_labels = [48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122]
+        model = Sequential()
+        model.add(Conv2D(filters=32, kernel_size=(3, 3), padding='valid', input_shape=(28, 28, 1), activation='relu'))
+        model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
+        model.add(Flatten())
+        model.add(Dense(512, activation='relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(len(emnist_labels), activation='softmax'))
+        model.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=['accuracy'])
+        X_train = idx2numpy.convert_from_file(emnist_path + '/emnist-byclass-train-images-idx3-ubyte')
+        y_train = idx2numpy.convert_from_file(emnist_path + '/emnist-byclass-train-labels-idx1-ubyte')
+
+        X_test = idx2numpy.convert_from_file(emnist_path + '/emnist-byclass-test-images-idx3-ubyte')
+        y_test = idx2numpy.convert_from_file(emnist_path + '/emnist-byclass-test-labels-idx1-ubyte')
+
+        X_train = np.reshape(X_train, (X_train.shape[0], 28, 28, 1))
+        X_test = np.reshape(X_test, (X_test.shape[0], 28, 28, 1))
+
+        print(X_train.shape, y_train.shape, X_test.shape, y_test.shape, len(emnist_labels))
+
+        k = 1 # 1=100% 10=10% 100=1% Колво файлов в эпохе
+        X_train = X_train[:X_train.shape[0] // k]
+        y_train = y_train[:y_train.shape[0] // k]
+        X_test = X_test[:X_test.shape[0] // k]
+        y_test = y_test[:y_test.shape[0] // k]
+
+        # Нормализуем
+        X_train = X_train.astype(np.float32)
+        X_train /= 255.0
+        X_test = X_test.astype(np.float32)
+        X_test /= 255.0
+
+        x_train_cat = keras.utils.to_categorical(y_train, len(emnist_labels))
+        y_test_cat = keras.utils.to_categorical(y_test, len(emnist_labels))
+        # Снижаем скорость обучения
+        learning_rate_reduction = keras.callbacks.ReduceLROnPlateau(monitor='val_accuracy', patience=3, verbose=1, factor=0.5, min_lr=0.00001)
+
+        model.fit(X_train, x_train_cat, validation_data=(X_test, y_test_cat), callbacks=[learning_rate_reduction], batch_size=64, epochs=30)
+
+        model.save('besedin_emnist.h5')
+
+    def imgtotext(self):
+        file,src = QFileDialog.getOpenFileName(self, "Pick file", ".", "*.jpg *.png *.jpeg") #Вызов проводника с аргументами 1-Текст, 2-Директория, 3-Формат
+        ChooseFile = file
+        #ChooseFile = '123.png'
+        self.label.setPixmap(QPixmap(ChooseFile)) #Передаем путь в функцию Qpixmap и выводим результат через .setPixmap
+        self.label.setScaledContents(True)
+        print(ChooseFile)
+        image_file = ChooseFile
+        img = cv2.imread(image_file)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY)
+        img_erode = cv2.erode(thresh, np.ones((3, 3), np.uint8), iterations=1)
+        emnist_labels = [48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122]
+
+        # Получение контура
+        contours, hierarchy = cv2.findContours(img_erode, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+        output = img.copy()
+
+        for idx, contour in enumerate(contours):
+            (x, y, w, h) = cv2.boundingRect(contour)
+            if hierarchy[0][idx][3] == 0:
+                cv2.rectangle(output, (x, y), (x + w, y + h), (70, 0, 0), 1)
+
+
+        #cv2.imshow("Input", img)
+        #cv2.imshow("Enlarged", img_erode)
+        cv2.imshow("Output", output)
+        cv2.waitKey(0)
+        def letters_extract(image_file: str, out_size=28) -> list[any]:
+            img = cv2.imread(image_file)
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY)
+            img_erode = cv2.erode(thresh, np.ones((3, 3), np.uint8), iterations=1)
+
+            # Получаем контуры
+            contours, hierarchy = cv2.findContours(img_erode, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+            output = img.copy()
+
+            letters = []
+            for idx, contour in enumerate(contours):
+                (x, y, w, h) = cv2.boundingRect(contour)
+                if hierarchy[0][idx][3] == 0:
+                    cv2.rectangle(output, (x, y), (x + w, y + h), (70, 0, 0), 1)
+                    letter_crop = gray[y:y + h, x:x + w]
+                    # print(letter_crop.shape)
+
+                    # Ресайз букв до квадрата
+                    size_max = max(w, h)
+                    letter_square = 255 * np.ones(shape=[size_max, size_max], dtype=np.uint8)
+                    if w > h:
+                        # Увеличиваем изображение сверху вниз
+                        y_pos = size_max//2 - h//2
+                        letter_square[y_pos:y_pos + h, 0:w] = letter_crop
+                    elif w < h:
+                        # Увеличиваем слева направо
+                        x_pos = size_max//2 - w//2
+                        letter_square[0:h, x_pos:x_pos + w] = letter_crop
+                    else:
+                        letter_square = letter_crop
+
+                    # Изменяем размер буквы до 28x28 и добавляем букву и ее координату X
+                    letters.append((x, w, cv2.resize(letter_square, (out_size, out_size), interpolation=cv2.INTER_AREA)))
+
+            # Сортируем по координате Х
+            letters.sort(key=lambda x: x[0], reverse=False)
+
+            return letters
+        letters = letters_extract(image_file)
+        cv2.imshow("0", letters[0][2])
+        cv2.imshow("1", letters[1][2])
+        cv2.imshow("2", letters[2][2])
+        cv2.imshow("3", letters[3][2])
+        cv2.imshow("4", letters[4][2])
+        cv2.waitKey(0)
+        
+        model = keras.models.load_model('besedin_emnist.h5') # Загружаем модель
+        
+        def emnist_predict_img(model, img):
+            img_arr = np.expand_dims(img, axis=0)
+            img_arr = 1 - img_arr/255.0
+            img_arr[0] = np.rot90(img_arr[0], 3)
+            img_arr[0] = np.fliplr(img_arr[0])
+            img_arr = img_arr.reshape((1, 28, 28, 1))
+
+            predict = model.predict([img_arr])
+            result = np.argmax(predict, axis=1)
+            return chr(emnist_labels[result[0]])
+        def img_to_str(model: any, image_file: str):
+            letters = letters_extract(image_file)
+            s_out = ""
+            for i in range(len(letters)):
+                dn = letters[i+1][0] - letters[i][0] - letters[i][1] if i < len(letters) - 1 else 0
+                s_out += emnist_predict_img(model, letters[i][2])
+                if (dn > letters[i][1]/4):
+                    s_out += ' '
+            return s_out
+        s_out = img_to_str(model, ChooseFile)
+        print(s_out)
 
 def application():
     app = QApplication(sys.argv) #Вызываем окно с доступом к консоли (sys.argv)
